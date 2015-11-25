298dd49ee8b6a5ec2dc0fb7751a358175f8e2667
Jonathan Chaney <jonchaney@gmail.com>
2015-11-25 15:06:46 -0800

    Initial Commit

---
+++ CS110C_Lab4/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  CS110C_Lab4
+//
+//  Created by Jon Chaney on 11/25/15.
+//  Copyright © 2015 Jon Chaney. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+  // insert code here...
+  std::cout << "Hello, World!\n";
+    return 0;
+}
